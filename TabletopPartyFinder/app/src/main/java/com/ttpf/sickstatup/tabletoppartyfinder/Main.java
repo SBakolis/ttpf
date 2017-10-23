71cea363abfd148bc30f948c4de7183f21613651
Nickkal17 <30806371+Nickkal17@users.noreply.github.com>
2017-10-24 02:28:15 +0300

    ChangeActivity

--- TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Main.java
+++ TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Main.java
@@ -6,46 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 
 
 public class Main extends AppCompatActivity {
-
-    Button FindGroupBtn = (Button)findViewById(R.id.menu_item_1);
-    Button HostGroupBtn = (Button)findViewById(R.id.menu_item_2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FindGroupBtn = (Button)findViewById(R.id.menu_item_1);
-        HostGroupBtn = (Button)findViewById(R.id.menu_item_2);
-
-        FindGroupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                passToFindGroup();
-            }
-        });
-
-        HostGroupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                passToHostGroup();
-            }
-        });
-
     }
 
-    public void passToFindGroup()
-    {
-        Intent findGrpActivity = new Intent(this, FindGroup.class);
-        startActivity(findGrpActivity);
+    public void findGroup(View view){
+        startActivity(new Intent(Main.this, FindGroup.class));
     }
 
-    public void passToHostGroup()
-    {
-        Intent hostGrpActivity = new Intent(this, HostGroup.class);
-        startActivity(hostGrpActivity);
+    public void hostGroup(View view){
+        startActivity(new Intent(Main.this, HostGroup.class));
     }
 }
