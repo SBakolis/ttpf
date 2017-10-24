ac092a0af45fa8cc2c1a255aa4f32af8866f5e88
zapatistas <30812664+zapatistas@users.noreply.github.com>
2017-10-25 02:35:34 +0300

    QQ

--- TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Main.java
+++ TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Main.java
@@ -3,6 +3,8 @@ package com.ttpf.sickstatup.tabletoppartyfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -15,7 +17,13 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
 
+        return true;
     }
 
     public void findGroup(View view){
