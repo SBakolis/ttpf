dd60f43ff8b7b3771326bed9194e6ac3fecf6f9c
zapatistas <30812664+zapatistas@users.noreply.github.com>
2017-10-25 11:15:55 +0300

    options menu try

--- TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Main.java
+++ TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Main.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -28,6 +29,22 @@ public class Main extends AppCompatActivity {
 >>>>>>> 53760507bdec405023b5a11d5d3637eaf21c6344
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                newGame();
+                return true;
+            case R.id.help:
+                showHelp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void findGroup(View view){
