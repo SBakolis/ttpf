2ea42565a7669118bae6df5798dd7676e56939f0
Nickkal17 <30806371+Nickkal17@users.noreply.github.com>
2017-10-27 02:57:29 +0300

    Update

--- TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/HostGroup.java
+++ TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/HostGroup.java
@@ -15,11 +15,14 @@ public class HostGroup extends Main {
         setContentView(R.layout.activity_host_group);
 
         Spinner gameSpinner = (Spinner)findViewById(R.id.games_spinner);
+        Spinner numberSpinner = (Spinner)findViewById(R.id.player_number_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.tabletop_games,R.layout.spinner_item_layout);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.number_of_players,R.layout.spinner_item_layout);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         gameSpinner.setAdapter(adapter);
+        numberSpinner.setAdapter(adapter2);
     }
 
     public void returnToMain(View view){
