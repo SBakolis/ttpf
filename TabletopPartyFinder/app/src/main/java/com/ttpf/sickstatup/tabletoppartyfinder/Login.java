52ba9909a24d70d44fa3c9cad97fada7b0f76a52
zapatistas <30812664+zapatistas@users.noreply.github.com>
2017-11-07 21:16:40 +0200

    login xml

---
+++ TabletopPartyFinder/app/src/main/java/com/ttpf/sickstatup/tabletoppartyfinder/Login.java
@@ -0,0 +1,24 @@
+package com.ttpf.sickstatup.tabletoppartyfinder;
+
+/**
+ * Created by zapatistas on 11/7/2017.
+ */
+
+package com.sourcey.materiallogindemo;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+      public class Login  extends
